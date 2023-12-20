d4d958e1a4099254bd933305e4094f54abd853fb
Yaseen Mohammad <yaseen@Yaseens-MacBook-Air.local>
2023-12-19 18:54:25 -0600

    added backend framework

---
+++ GrowerPortal/src/main/java/com/growerportal/GrowerPortal/GrowerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.growerportal.GrowerPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowerPortalApplication.class, args);
+	}
+
+}
