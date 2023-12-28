2fafcba468f8fe5eda71f35886905c4d66520ca7
Yaseen Mohammad <yaseen@Yaseens-MacBook-Air.local>
2023-12-27 22:18:50 -0600

    implemented email service and html templates

--- GrowerPortal/src/main/java/com/growerportal/GrowerPortal/service/impl/EmailServiceImpl.java
+++ GrowerPortal/src/main/java/com/growerportal/GrowerPortal/service/impl/EmailServiceImpl.java
@@ -4,11 +4,20 @@ import com.growerportal.GrowerPortal.service.EmailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Base64;
 
 @Service
@@ -17,10 +26,26 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Value("${notification.srteam.emails}")
+    private String[] recipients;
 
     public void sendVerificationEmail(String farmerEmail, String otp) {
         String htmlContent = buildOtpEmail(otp);
         this.sendEmail(farmerEmail, "Growers Portal - Verify Your Email", htmlContent);
+    }
+
+    private String buildOtpEmail(String otp) {
+        try {
+            Resource resource = resourceLoader.getResource("classpath:templates/otp-verification-email.html");
+            String content = new String(Files.readAllBytes(Paths.get(resource.getURI())));
+            return content.replace("{{ otp }}", otp);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public String generateOtp() {
@@ -30,16 +55,6 @@ public class EmailServiceImpl implements EmailService {
         return Base64.getUrlEncoder().withoutPadding().encodeToString(values).substring(0, 6);
     }
 
-    public String buildOtpEmail(String otp) {
-        return "<html>" +
-                "<body>" +
-                "<h1>Growers Portal - Email Verification</h1>" +
-                "<p>Your OTP for email verification is:</p>" +
-                "<h2 style='color: blue;'>" + otp + "</h2>" +
-                "<p>Please use this code to complete your registration.</p>" +
-                "</body>" +
-                "</html>";
-    }
 
     public void sendEmail(String to, String subject, String htmlContent) {
         try {
@@ -51,6 +66,22 @@ public class EmailServiceImpl implements EmailService {
             mailSender.send(mimeMessage);
         } catch (MessagingException e) {
             throw new RuntimeException("Failed to send email: " + e.getMessage());
+        }
+    }
+
+
+    public void notifySrTeam(String name, String email, String date) {
+        String subject = "New Form Submission Received";
+        Resource resource = resourceLoader.getResource("classpath:templates/notify-sr-email.html");
+        String htmlContent;
+        try {
+            htmlContent = new String(Files.readAllBytes(Paths.get(resource.getURI())), StandardCharsets.UTF_8);
+            htmlContent = htmlContent.replace("[Name]", name)
+                    .replace("[Email]", email)
+                    .replace("[Date]", date);
+            sendEmail(Arrays.toString(recipients), subject, htmlContent);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load email template: " + e.getMessage());
         }
     }
 
