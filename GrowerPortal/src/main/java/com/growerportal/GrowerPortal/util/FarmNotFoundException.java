6787530bc628cf9114afb57682aed79b54d606df
Yaseen Mohammad <yaseen@Yaseens-MacBook-Air.local>
2024-02-01 22:49:09 -0600

    merged fine with main

--- GrowerPortal/src/main/java/com/growerportal/GrowerPortal/util/FarmNotFoundException.java
+++ GrowerPortal/src/main/java/com/growerportal/GrowerPortal/util/FarmNotFoundException.java
@@ -2,6 +2,7 @@ package com.growerportal.GrowerPortal.util;
 
 public class FarmNotFoundException extends RuntimeException {
     public FarmNotFoundException(Long farmId) {
-        super(STR."Farm not found with ID: \{farmId}");
+
+        super(String.format("Farm not found with ID: %d", farmId));
     }
 }
